04a6919d589bcf45485cf51db5416b2b468476f6
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2022-11-20 20:05:02 +0800

    add comment

--- tugas_3/no1.py
+++ tugas_3/no1.py
@@ -5,6 +5,7 @@ def z(x1,x2):
 
 table = PrettyTable()
 
+# menampilkan table untuk tableau
 def refreshTable():
   basic = determineBasics()
   table.clear()
@@ -13,13 +14,12 @@ def refreshTable():
     table.add_row(x)
   
   table._field_names.insert(0, "Basic")
-  table._align["Basic"] = 'c'
-  table._valign["Basic"] = 't'
   
   for i, _ in enumerate(table._rows):
     table._rows[i].insert(0, basic[i])
   print(table)
 
+# menampilkan table tanpa kolom ratio
 def refreshTableNoR():
   basic = determineBasics()
   table.clear()
@@ -34,6 +34,7 @@ def refreshTableNoR():
     table._rows[i].insert(0, basic[i])
   print(table)
 
+# menentukan basis dari kanonik
 def determineBasics():
   basicIndex = []
   basic = []
@@ -64,6 +65,7 @@ class SimplexMethod:
     refreshTableNoR()
     print()
   
+  # Mencari nilai paling kecil pada kanonik
   def findMinValue(self):
     minValue = []
     for x in self.b:
@@ -74,47 +76,57 @@ class SimplexMethod:
   def calculateTableau(self):
     refreshTable()
     minValue = self.findMinValue()
-    key = 0
-    index = 0
+    key = 0 #key untuk mencari baris ratio terkecil
+    index = 0 #posisi unsur kunci
     if minValue in self.b[0]:
       index = self.b[0].index(minValue)
+      # menghitung ratio kanonik
       self.b[1][-1] = self.b[1][-2] / self.b[1][index]
       self.b[2][-1] = self.b[2][-2] / self.b[2][index]
 
-      minRatio = min(self.b[1][-1], self.b[2][-1])
+      # mencari nilai ratio terkecil
+      minRatio = min(self.b[1][-1], self.b[2][-1]) 
+      # menentukan baris ratio terkecil
       key = 1 if minRatio in self.b[1] else 2
     elif minValue in self.b[1]:
       index = self.b[1].index(minValue)
+      # menghitung ratio kanonik
       self.b[0][-1] = self.b[0][-2] / self.b[0][index]
       self.b[2][-1] = self.b[2][-2] / self.b[2][index]
       
+      # mencari nilai ratio terkecil
       minRatio = min(self.b[0][-1], self.b[2][-1])
+      # menentukan baris ratio terkecil
       key = 0 if minRatio in self.b[0] else 2
     elif minValue in self.b[2]:
       index = self.b[2].index(minValue)
+      # menghitung ratio kanonik
       self.b[0][-1] = self.b[0][-2] / self.b[0][index]
       self.b[1][-1] = self.b[1][-2] / self.b[1][index]
 
+      # mencari nilai ratio terkecil
       minRatio = min(self.b[0][-1], self.b[1][-1])
+      # menentukan baris ratio terkecil
       key = 0 if minRatio in self.b[0] else 1
     
     print(f"\nKolom Kunci: Kolom-{index+1}, Unsur Kunci: {b[key][index]} (b{index}), Ratio Terkecil: {minRatio}")
     refreshTable()
-    self.obd(key,index)
+    self.obd(key,index) #melakukan operasi baris dasar
 
   def obd(self, key, index):
     print("\nOBD")
-    div = self.b[key][index]
+    div = self.b[key][index] #pembagi unsur kunci menjadi 1
     print(f"1/{div} * b{key}")
+    #membagi seluruh baris unsur kunci
     for i in range(len(self.b[key])-1):
       self.b[key][i] = self.b[key][i]/div
 
-    timesBy = self.b[key-1][index]
+    timesBy = self.b[key-1][index] #penentu pembentuk 0
     print(f"b{key-1} - {timesBy}b{key}")
     for i in range(len(self.b[key])-1):
       self.b[key-1][i] = self.b[key-1][i] - (timesBy*self.b[key][i])
     
-    timesBy = self.b[key-2][index]
+    timesBy = self.b[key-2][index] #penentu pembentuk 0
     print(f"b{2 if key-2 == -1 else 0} - {timesBy}b{key}")
     for i in range(len(self.b[key])-1):
       self.b[key-2][i] = self.b[key-2][i] - (timesBy*self.b[key][i])
@@ -122,10 +134,10 @@ class SimplexMethod:
     self.clearRatio()
     refreshTableNoR()
 
+  #membuat seluruh nilai ratio menjadi 0
   def clearRatio(self):
     for i in range(len(self.b)):
       self.b[i][-1] = 0
-
 
   def solve(self):
     x1 = x2 = 0
@@ -134,14 +146,11 @@ class SimplexMethod:
       print(f"\033[31mTableau {n}\033[0m")
       self.calculateTableau()
       print()
-
       if self.b[0][0] == 1 and self.b[1][1] == 1 and self.b[2][2] == 1:
         x1 = self.b[1][-2]
         x2 = self.b[2][-2]
         break
-      
       n += 1
-    
     print("Didapatkan : ")
     print(f"x1 = {x1}")
     print(f"x2 = {x2}")
